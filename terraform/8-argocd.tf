87d994969694425367cd1e951b4242000a37227e
Dmitry Fal <fal.dmitry@gmail.com>
2023-12-02 22:44:39 +0200

    Uncomment ingress-argocd terraform resource

--- terraform/8-argocd.tf
+++ terraform/8-argocd.tf
@@ -8,33 +8,33 @@ resource "helm_release" "argocd" {
   values           = [file("${path.module}/values/argocd.yaml")]
 }
 
-#resource "kubernetes_ingress" "external_argo_ui" {
-#  wait_for_load_balancer = true
-#  metadata {
-#    name      = "external-argo-ui"
-#    namespace = "argocd"
-#    annotations = {
-#      "kubernetes.io/ingress.class" = "nginx"
-#    }
-#  }
+resource "kubernetes_ingress" "ingress_argocd" {
+  metadata {
+    name      = "ingress-argocd"
+    namespace = "argocd"
+    annotations = {
+      "kubernetes.io/ingress.class" = "external-nginx"
+    }
+  }
 
-#  depends_on = [helm_release.external_nginx, helm_release.argocd]
-#  spec {
-#    rule {
-#      host = ""
-#      http {
-#        path {
-#          path = "/argocd"
-#          backend {
-#            service_name = "argocd-server"
-#            service_port = 80
-#          }
-#        }
-#      }
-#    }
-#  }
-#}
+  spec {
+    rule {
+      host = ""
+      http {
+        path {
+          path = "/argocd/*"
+          backend {
+            service_name = "argocd-server"
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
 
-#output "load_balancer_hostname" {
-#  value = kubernetes_ingress.external_argo_ui.status.0.load_balancer.0.ingress.0.hostname
-#}
+  wait_for_load_balancer = true
+}
+
+output "load_balancer_hostname" {
+  value = kubernetes_ingress.ingress_argocd.status.0.load_balancer.0.ingress.0.hostname
+}
