be98ae5ecf58fb724f810a736a7247d4c7fbc951
Dmitry Fal <fal.dmitry@gmail.com>
2023-12-05 22:33:34 +0200

    Add ArgoCD Application for staging

--- terraform/8-argocd.tf
+++ terraform/8-argocd.tf
@@ -7,34 +7,3 @@ resource "helm_release" "argocd" {
   version          = "5.51.4"
   values           = [file("${path.module}/values/argocd.yaml")]
 }
-
-resource "kubernetes_ingress" "ingress_argocd" {
-  metadata {
-    name      = "ingress-argocd"
-    namespace = "argocd"
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-    }
-  }
-
-  spec {
-    rule {
-      host = ""
-      http {
-        path {
-          path = "/argocd/*"
-          backend {
-            service_name = "argocd-server"
-            service_port = 80
-          }
-        }
-      }
-    }
-  }
-
-  wait_for_load_balancer = true
-}
-
-output "load_balancer_hostname" {
-  value = kubernetes_ingress.ingress_argocd.status.0.load_balancer.0.ingress.0.hostname
-}
